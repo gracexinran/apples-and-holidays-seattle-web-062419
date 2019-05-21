0d299df8e6eb063afb21410615d3349821e1b6d4
Xinran Che <gracexinran@gmail.com>
2019-05-21 20:05:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday_hash[season].each do |day, supply|
-      puts "#{day.to_s.split.capitalize.join(' ')}: #{day.split capitalize}: #{supply.join(', ')}"
+      puts "#{day.to_s.split(-).join(' ').capitalize}: #{day.split capitalize}: #{supply.join(', ')}"
     end
   end
   
